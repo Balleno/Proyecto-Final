ab775a621b5b6e2b35d1b6cb2bc17d1980bbac0a
Balleno <pedrolorenzovega@gmail.com>
2022-06-07 19:49:59 +0200

    commit target_charset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('Comparador') }}</title>
+    <title>Comparador</title>
 
     <!-- Scripts -->
    
