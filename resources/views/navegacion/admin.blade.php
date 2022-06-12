18f634ec9a03b7fe46a52e0a7018f4e4230e5dca
Balleno <pedrolorenzovega@gmail.com>
2022-06-12 16:48:28 +0200

    commit usuario_datos

--- resources/views/navegacion/admin.blade.php
+++ resources/views/navegacion/admin.blade.php
@@ -11,7 +11,7 @@
 
                     @method('POST')
                         <table class="tablausuario">
-                            <tr><a href={{ $usuario->nombre }} target="_blank">{{ $usuario->nombre }}</a></tr>
+                            <tr><a href={{ $usuario->name }} target="_blank">{{ $usuario->name }}</a></tr>
                             <tr>
                                 <button type="submit" class="btn btn-primary">
                                     Borrar
